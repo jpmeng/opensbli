0cda5b05745efba761b8584633ad8bd8b13187f4
jpmeng <jpmeng@gmail.com>
2023-01-30 16:30:05 +0000

    Improve Translate.sh for better options

--- opensbli/work_flow/Translate.sh
+++ opensbli/work_flow/Translate.sh
@@ -1,19 +1,56 @@
+#!/bin/bash
 ##@brief Translate a OpenSBLI app to OPS C/C++ code
 ##@author Jianping Meng
 ##@contributors
 ##@details
-#!/bin/bash
-if [ $# -ne 2 ]
+
+function usage {
+    echo "This script will translate the OpenSBLI Python cod  e to C/C++!"
+    echo "Usage: ./$(basename $0) PythonSource"
+    echo "./$(basename $0) -h -> showing usage"  
+    echo "./$(basename $0) -e -> Specifying the environment directory."
+}
+
+ScriptPath="${BASH_SOURCE:-$0}"
+AbsolutScriptPath="$(realpath "${ScriptPath}")"
+EnvDir="$(dirname "${AbsolutScriptPath}")"
+
+optstring="e:h"
+
+
+while getopts ${optstring} options; do
+    case ${options} in
+        h)
+            usage
+            exit 0
+        ;;       
+        e)
+            EnvDir=${OPTARG}
+        ;;       
+        :)
+            echo "$0: Must supply an argument to -$OPTARG." >&2
+            exit 1
+        ;;
+        ?)
+            echo "Invalid option: -${OPTARG}."
+            exit 2
+        ;;
+    esac
+done
+
+Src=${@:$OPTIND:1}
+
+if [ -z $Src ]
 then
-    echo "This script will create the whole OpenSBLI environment !"
-    echo "Usage: ./translate.sh EnvDir source "
-    echo "EnvDir: the directory for the environment."
-    echo "src: python source file"
-    exit 1
+    echo "This script will translate the OpenSBLI Python code to C/C++!"
+    echo "Usage: ./$(basename $0) [options] PythonSource"
+    echo "./$(basename $0) -h -> showing usage"
+    echo "./$(basename $0) -e -> Specifying the environment directory."
 fi
-EnvDir=$1
-src=$2
- # activate Python2
-source $1/Python2/bin/activate
-export PYTHONPATH=$PYTHONPATH:$1/opensbli-cpc_release
-python $src
+
+
+echo "Source name is ${Src} and evn is ${EnvDir}"
+# activate Python
+source ${EnvDir}/Python/bin/activate "${EnvDir}/Python"
+export PYTHONPATH=$PYTHONPATH:$EnvDir/OpenSBLI
+python $Src
